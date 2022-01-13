1f49f52cf512f59a11a5ae41b3cd83bfde75c623
deddybrama11 <dedybramayadi@gmail.com>
2022-01-13 14:16:28 +0700

    setting up environtment and create contrats and change name of image

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Kryptobird {
+    
+    string public name;
+    string private symbol;
+
+    constructor() {
+        name = "Kryptobird";
+        symbol = "KBIRDZ";
+    }
+}
