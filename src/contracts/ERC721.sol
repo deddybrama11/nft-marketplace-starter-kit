53dcb794c6a65040523cbe2964d62c485ac4fed5
deddybrama11 <dedybramayadi@gmail.com>
2022-01-17 16:04:56 +0700

    add ERC165 interface and optional approval on ERC721

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721 {
+import './ERC165.sol';
+
+contract ERC721 is ERC165{
 
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed tokenId
+    );
     /*
         BUILDING OUT THE MINTING FUNCTION :
         A. NFT TO POINT TO AN ADDRES
@@ -18,6 +26,9 @@ contract ERC721 {
 
      // Mapping from owner to number of owned tokens
      mapping(address => uint256) private _OwnedTokensCount;
+
+    //Mapping from token id to approved addresses
+    mapping(uint256 => address) private _tokenApprovals;
 
 
     /// @notice Count all NFTs assigned to an owner
@@ -38,7 +49,7 @@ contract ERC721 {
     /// @param _tokenId The identifier for an NFT
     /// @return The address of the owner of the NFT
 
-    function ownerOf(uint256 _tokenId) external view returns (address){
+    function ownerOf(uint256 _tokenId) public view returns (address){
         address owner = _tokenOwner[_tokenId];
         require(owner != address(0), 'owner query for non-existent to address');
         return owner;
@@ -64,4 +75,55 @@ contract ERC721 {
 
          emit Transfer(address(0), to, tokenId);
      }
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal{
+        // a. require that the address receiving a token is not a rezo address
+        require(_to != address(0),'Error - ERC721 Transfer to zero address');
+        require(ownerOf(_tokenId) == _from, 'Trying to transfer a token the address does not own!');
+
+        // 2. update the balance of the address _from token
+        _OwnedTokensCount[_from] -= 1;
+        // 3. update the balance of the address _to
+        _OwnedTokensCount[_to] += 1;
+        // 1. add the token id to the address receiving the token
+        _tokenOwner[_tokenId] = _to;
+
+        emit Transfer(address(0), _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public{
+        require(isApprovedOrOwner(msg.sender, _tokenId));
+        _transferFrom(_from, _to, _tokenId);
+    }
+
+    // 1. require that the person approving is the owner
+    // 2. we are approving an address to a token (tokenId)
+    // 3. require that we cant approve sending tokens of the owner to the owner
+    // 4. update the map of the approval addresses
+
+    function approve(address _to, uint256 tokenId) public{
+        address owner = ownerOf(tokenId);
+        require(_to != owner, 'Error - approval to current owner');
+        require(msg.sender == owner, 'Current caller is not the owner of the token');
+        _tokenApprovals[tokenId] = _to;
+
+        emit Approval(owner, _to, tokenId);
+    }
+
+    function isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool){
+        require(_exists(tokenId), 'token does not exists');
+        address owner = ownerOf(tokenId);
+        return (spender == owner);
+    }
 }
--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -8,7 +8,7 @@ contract ERC721Enumerable is ERC721 {
 
     // CHALLANGE !
     // mapping from tokenId to position in _allTokens array
-    mapping(uint256 => uint256) private _tokenIdToPositionToken;
+    mapping(uint256 => uint256) private _allTokensIndex;
 
     // mapping of owner to list of all owner token ids
     mapping(address => uint256[]) private _ownedTokens;
@@ -16,21 +16,44 @@ contract ERC721Enumerable is ERC721 {
     // mapping from token ID to index of the owner tokens list 
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
- 
-    // function tokenByIndex(uint256 _index) external view returns (uint256);
+    // two functions - one that returns tokenByIndex and
+    // another one that returns tokenOfByOwnerIndex
 
+    function tokenByIndex(uint256 _index) public view returns (uint256){
+        // make sure that the index is not out of bounds of the 
+        // total supply
+        require(totalSupply() > _index, 'global index is out of bounds!');
+        return _allTokens[_index];
+    }
 
-    // function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256){
+        require(_index < balanceOf(_owner), 'owner index is out of bounds!');
+        return _ownedTokens[_owner][_index];
+    }
 
     function _mint(address to, uint256 tokenId) internal override(ERC721){
         super._mint(to, tokenId);
-        _addTokenToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
     }
 
-    function _addTokenToTotalSupply(uint256 tokenId) private {
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        // EXERCISE
+        
+        // 2. ownedTokensIndex tokenId set to address of ownedTokens position
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        
+        // 1. add address and tokenId to _ownedToken
+        _ownedTokens[to].push(tokenId);
+
+    }
+
+    // add tokens to the _alltokens array and set the position of the tokens indexes
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
     }
-
+    // return the total supply of the _allTokens array
     function totalSupply() public view returns(uint256){
         return _allTokens.length;
     }
