7f742d141e46b7b809f19d310138412dca2cde27
deddybrama11 <dedybramayadi@gmail.com>
2022-01-18 20:17:11 +0700

    finishing test

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,52 +1,75 @@
-const {assert} = require('chai')
+const { assert } = require("chai");
 
-const KryptoBird = artifacts.require('./KryptoBird');
+const KryptoBird = artifacts.require("./KryptoBird");
 
 // check for chai
-require('chai')
-.use(require('chai-as-promised'))
-.should()
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
 
-contract('KryptoBird', (accounts) => {
-    let contract
-    // before tells our test to run this first before anything else
-    before( async ()=>{
-            contract = await KryptoBird.deployed()
-        }
-    );
-    // testing container - describe
-    describe('deployment', async ()=> {
-        // test samples with writing it 
-        it('deploys successfully', async() => {
-            const address = contract.address;
+contract("KryptoBird", (accounts) => {
+  let contract;
+  // before tells our test to run this first before anything else
+  before(async () => {
+    contract = await KryptoBird.deployed();
+  });
+  // testing container - describe
+  describe("deployment", async () => {
+    // test samples with writing it
+    it("deploys successfully", async () => {
+      const address = contract.address;
 
-            assert.notEqual(address, '');
-            assert.notEqual(address, null);
-            assert.notEqual(address, undefined);
-            assert.notEqual(address, '0x0');
-        })
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+      assert.notEqual(address, "0x0");
+    });
 
-        it('has a name and symbol', async() => {
-            const name = await contract.name();
-            const symbol = await contract.symbol();
-            assert.equal(name, 'KryptoBird')
-            assert.equal(symbol, 'KBIRDZ')
-        })
-    })
-    describe('minting', async() => {
-        it('creates a new token', async ()=> {
-            const result = await contract.mint('https...1')
-            const totalSupply = await contract.totalSupply()
+    it("has a name and symbol", async () => {
+      const name = await contract.name();
+      const symbol = await contract.symbol();
+      assert.equal(name, "KryptoBird");
+      assert.equal(symbol, "KBIRDZ");
+    });
+  });
 
-            //Success
-            assert.equal(totalSupply, 1)
-            const event = result.logs[0].args
-            assert.equal(event._from, '0x0000000000000000000000000000000000000000','from is the contract')
-            assert.equal(event._to, accounts[0], 'to is msg.sender')
+  describe("minting", async () => {
+    it("creates a new token", async () => {
+      const result = await contract.mint("https...1");
+      const totalSupply = await contract.totalSupply();
 
-            // Failure
-            await contract.mint('https...1').should.be.rejected;
+      //Success
+      assert.equal(totalSupply, 1);
+      const event = result.logs[0].args;
+      assert.equal(
+        event._from,
+        "0x0000000000000000000000000000000000000000",
+        "from is the contract"
+      );
+      assert.equal(event._to, accounts[0], "to is msg.sender");
 
-        })
-    })
-})
+      // Failure
+      await contract.mint("https...1").should.be.rejected;
+    });
+  });
+
+  describe("indexing", async () => {
+    it("lists KryptoBirdz", async () => {
+      await contract.mint("https...2");
+      await contract.mint("https...3");
+      await contract.mint("https...4");
+      const totalSupply = await contract.totalSupply();
+
+      // Loop through list and grab KBirdz from list
+      let result = [];
+      let KryptoBird;
+      for (i = 1; i <= totalSupply; i++) {
+        KryptoBird = await contract.kryptoBirdz(i - 1);
+        result.push(KryptoBird);
+      }
+      // assert that our new array result will equal our expected result
+      let expected = ["https...1", "https...2", "https...3", "https...4"];
+      assert.equal(result.join(","), expected.join(","));
+    });
+  });
+});
