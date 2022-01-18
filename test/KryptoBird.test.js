c997c5c923e016e8f379eafa1017c53488de13a9
deddybrama11 <dedybramayadi@gmail.com>
2022-01-18 16:53:40 +0700

    add testing

---
+++ test/KryptoBird.test.js
@@ -0,0 +1,52 @@
+const {assert} = require('chai')
+
+const KryptoBird = artifacts.require('./KryptoBird');
+
+// check for chai
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('KryptoBird', (accounts) => {
+    let contract
+    // before tells our test to run this first before anything else
+    before( async ()=>{
+            contract = await KryptoBird.deployed()
+        }
+    );
+    // testing container - describe
+    describe('deployment', async ()=> {
+        // test samples with writing it 
+        it('deploys successfully', async() => {
+            const address = contract.address;
+
+            assert.notEqual(address, '');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
+            assert.notEqual(address, '0x0');
+        })
+
+        it('has a name and symbol', async() => {
+            const name = await contract.name();
+            const symbol = await contract.symbol();
+            assert.equal(name, 'KryptoBird')
+            assert.equal(symbol, 'KBIRDZ')
+        })
+    })
+    describe('minting', async() => {
+        it('creates a new token', async ()=> {
+            const result = await contract.mint('https...1')
+            const totalSupply = await contract.totalSupply()
+
+            //Success
+            assert.equal(totalSupply, 1)
+            const event = result.logs[0].args
+            assert.equal(event._from, '0x0000000000000000000000000000000000000000','from is the contract')
+            assert.equal(event._to, accounts[0], 'to is msg.sender')
+
+            // Failure
+            await contract.mint('https...1').should.be.rejected;
+
+        })
+    })
+})
